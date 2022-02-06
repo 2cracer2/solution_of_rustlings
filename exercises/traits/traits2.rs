a57d8471a7929793750abbaca5cf52a1265b60a4
Cracer <cracer10@foxmail.com>
2022-02-06 22:53:09 +0800

    start

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -18,6 +18,13 @@ trait AppendBar {
 
 //TODO: Add your code here
 
+impl AppendBar for vec<String> {
+    fn append_bar(self) -> Self {
+        let mut new = self;
+        new.push("Bar".into());
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
